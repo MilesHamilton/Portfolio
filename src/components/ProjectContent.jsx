d62c09f2a96572327496892f932b151593fbcbcb
Ubuntu <mham7714@gmail.com>
2022-08-02 18:14:24 +0000

    new project added

--- src/components/ProjectContent.jsx
+++ src/components/ProjectContent.jsx
@@ -1,95 +1,91 @@
-import React, { Component } from 'react';
-import './CSS/projectContent.css';
-import { Link } from 'react-router-dom';
+import React, { Component } from "react"
+import "./CSS/projectContent.css"
+import { Link } from "react-router-dom"
 
 export class ProjectContent extends Component {
-  constructor(props) {
-    super(props);
-  }
+	constructor(props) {
+		super(props)
+	}
 
-  render() {
-    return (
-      <div>
-        <nav className=' z-depth-0 white router-nav'>
-          <Link to='/home'>
-            <a href='#' className='brand-logo left'>
-              <div className='logo router-logo'>MH</div>
-            </a>
-          </Link>
-          <div className='ProjectContent_title'>
-            <h3>{this.props.title}</h3>
-          </div>
-        </nav>
-        <div className='container'>
-          <div className='col s12'>
-            <div className='ProjectContent_wrapper'>
-              <div className='ProjectContent_links'>
-                <div className='ProjectContent_site'>
-                  <a
-                    href={this.props.link}
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    className='ProjectContent__info'
-                  >
-                    Visit Site
-                  </a>
-                </div>
-                <div className='ProjectContent_github'>
-                  <a
-                    href={this.props.github}
-                    rel='noopener noreferrer'
-                    target='_blank'
-                    className='ProjectContent__info'
-                  >
-                    Visit Github
-                  </a>
-                </div>
-              </div>
+	render() {
+		return (
+			<div>
+				<nav className=" z-depth-0 white router-nav">
+					<Link to="/home">
+						<a href="#" className="brand-logo left">
+							<div className="logo router-logo">MH</div>
+						</a>
+					</Link>
+					<div className="ProjectContent_title">
+						<h3>{this.props.title}</h3>
+					</div>
+				</nav>
+				<div className="container">
+					<div className="col s12">
+						<div className="ProjectContent_wrapper">
+							<div className="ProjectContent_links">
+								<div className="ProjectContent_site">
+									<a
+										href={this.props.link}
+										rel="noopener noreferrer"
+										target="_blank"
+										className="ProjectContent__info"
+									>
+										Visit Site
+									</a>
+								</div>
+								<div className="ProjectContent_github">
+									<a
+										href={this.props.github}
+										rel="noopener noreferrer"
+										target="_blank"
+										className="ProjectContent__info"
+									>
+										Visit Github
+									</a>
+								</div>
+							</div>
 
-              <div className='ProjectContent_gif'>
-                <img src={this.props.gif}></img>
-              </div>
-              <div className='ProjectContent_description'>
-                <h6>{this.props.descriptionTitle}</h6>
-                <p>{this.props.description}</p>
-              </div>
-              <div className='ProjectContent_infowrapper'>
-                <div className='ProjectContent_challenge'>
-                  <p>
-                    <strong>Challenges:</strong>
-                  </p>
-                  <ul>
-                    <p>
-                      {this.props.challenges.map((elm) => {
-                        return (
-                          <li>
-                            ✦ {''} {elm.challenge}
-                          </li>
-                        );
-                      })}
-                    </p>
-                  </ul>
-                </div>
+							<div className="ProjectContent_gif">
+								<img src={this.props.gif}></img>
+							</div>
+							<div className="ProjectContent_description">
+								<h6>{this.props.descriptionTitle}</h6>
+								<p>{this.props.description}</p>
+							</div>
+							<div className="ProjectContent_infowrapper">
+								<div className="ProjectContent_challenge">
+									<p>
+										<strong>Challenges:</strong>
+									</p>
+									<ul>
+										<p>
+											{this.props.challenges.map((elm) => {
+												return <li>✦ {elm.challenge}</li>
+											})}
+										</p>
+									</ul>
+								</div>
 
-                <div className='ProjectContent_tech'>
-                  <p>
-                    <strong>Stack:</strong>
-                  </p>
-                  <ul>
-                    <p>
-                      {this.props.tech.map((elm) => {
-                        return <li>{elm.tech}</li>;
-                      })}
-                    </p>
-                  </ul>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-    );
-  }
+								<div className="ProjectContent_tech">
+									<p>
+										<strong>Stack:</strong>
+									</p>
+									<ul>
+										<p>
+											{this.props.tech.map((elm) => {
+												return <li>{elm.tech}</li>
+											})}
+										</p>
+									</ul>
+								</div>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		)
+	}
 }
 
-export default ProjectContent;
+export default ProjectContent
--- src/pages/MovieAndTv.jsx
+++ src/pages/MovieAndTv.jsx
@@ -1,29 +1,36 @@
-import React from 'react';
-import ProjectContent from '../components/ProjectContent';
+import React from "react"
+import ProjectContent from "../components/ProjectContent"
 
 export default function MovieAndTv() {
-  return (
-    <div className='page'>
-      <ProjectContent
-        title='Movie and Tv Finder'
-        link='https://movieandtvfinder.vercel.app/'
-        gif={require('../components/CSS/cinema.jpg')}
-        descriptionTitle='For your Viewing Pleasure'
-        description='Using data from the API site "The Movie Database", this app spits out a random movie or tv show complete with a description and score whenever you hit the button'
-        challenges={[
-          { challenge: 'Utilizing and connecting an API for the first time' },
-          {
-            challenge:
-              'In order to have access to 1000s of movies supplied by "The Movie Database" I needed to impliment frontend pagination',
-          },
-          {
-            challenge: 'Implementing a the UX feature "tabs" into the website ',
-          },
-        ]}
-        tech={[{ tech: 'TMDB API' }, { tech: 'React.js' }]}
-        github='https://github.com/MilesHamilton/Movies-and-Tv-Finder'
-        link='https://movieandtvfinder.vercel.app/'
-      />
-    </div>
-  );
+	return (
+		<div className="page">
+			<ProjectContent
+				title="Flixter"
+				gif={require("../components/CSS/cinema.jpg")}
+				descriptionTitle="For your Viewing Pleasure"
+				description="Combining the power of Spotify and The Movie Database this app allows you to discover popular movie and tv soundtracks quickly"
+				challenges={[
+					{
+						challenge:
+							"Utilizing and connecting Spotify API and using their Oauth for anyone with a spotify account to acces their services",
+					},
+					{
+						challenge:
+							'In order to have access to 1000s of movies supplied by "The Movie Database" I needed to impliment frontend pagination',
+					},
+					{
+						challenge:
+							"The implementation of Typescript which helped with propogating functions throughout the app and assigning types to state variables",
+					},
+				]}
+				tech={[
+					{ tech: "TMDB API" },
+					{ tech: "React.js" },
+					{ tech: "Spotify API" },
+				]}
+				github="https://github.com/MilesHamilton/Movies-and-Tv-Finder"
+				link="https://flixter.app"
+			/>
+		</div>
+	)
 }
